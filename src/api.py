10d4ad7e38453559f798ef562fec11febbcc3918
git_rtk <17_ufo_RB@proton.me>
2024-05-25 12:00:33 +0200

    update .gitignore filled out .src/api.py

---
+++ src/api.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+import requests, json
+
+class BaseAPI(ABC):
+    @abstractmethod
+    def get_vacancies(self, keyword, vac_qty):
+        pass
+
+class HHApi(BaseAPI):
+    """Класс для осуществления подключения к API службе внешнего источника данных согласно заданных параметров"""
+    def __init__(self):
+        self.url = 'https://api.hh.ru/vacancies'
+        self.params = {
+            'text': '',
+            'page': 0,
+            'per_page': 100
+        }
+        # pass
+
+    def get_vacancies(self, keyword, vac_qty):
+        """Метод для загрузки данных согласно сутруктуры запроса"""
+        self.params.update({'text': keyword, 'per_page': vac_qty})
+        response = requests.get(self.url, params=self.params)
+        return response.json()['items']
+        # pass
+
+
+hh_api = HHApi()
+"""Вызов экземпляра класса"""
